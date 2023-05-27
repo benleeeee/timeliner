e6fcf79c39fc3567869c776419d6059ed33b1c32
Ben Lee <leeben118@gmail.com>
2023-05-27 20:53:12 +0100

    it's a mess because I don't understand binding, but I have some basic structure for the media type classes that encapsulate everything needed to draw a timeline

--- Timeliner/MainWindow.xaml.cs
+++ Timeliner/MainWindow.xaml.cs
@@ -7,22 +7,78 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Timeliner.MediaTypes;
 
 namespace Timeliner
 {
+    //public struct Line
+    //{
+    //    public float x1;
+    //    public float x2;
+    //    public SolidColorBrush colour;
+    //    public float strokeThickness;
+    //}
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Media> mediaList = new List<Media>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = this;
+
+            FakeInput();
+        }
+
+
+        /// <summary>
+        /// Fake list of media for now
+        /// TODO: Read from a file the media information
+        /// </summary>
+        public void FakeInput()
+        {
+            //graphGridContainer.DataContext = mediaList;
+
+            //Construct some media and add to list
+            Media GotG3 = new Movie();
+            GotG3.startDateTime = new DateTime(2023, 5, 25);
+            GotG3.finishDateTime = new DateTime(2023, 5, 25);
+            GotG3.name = "GotG3";
+
+            Media Zom100_V1 = new Manga();
+            Zom100_V1.startDateTime = new DateTime(2023, 5, 25);
+            Zom100_V1.finishDateTime = new DateTime(2023, 5, 26);
+            Zom100_V1.name = "Zom100_V1";
+
+            mediaList.Add(GotG3);
+            mediaList.Add(Zom100_V1);
+
+
+            
+
+            Line line = new Line { X1 = 0, X2 = 100, Y1 = 0, Y2 = 100, Stroke = Brushes.Black, StrokeThickness = 2 };
+
+            //graphGrid.Children.Add(line);
+        }
+
+        private void AddToFakeList(object sender, RoutedEventArgs e)
+        {
+            Media Zom100_V1 = new Manga();
+            Zom100_V1.startDateTime = new DateTime(2023, 5, 25);
+            Zom100_V1.finishDateTime = new DateTime(2023, 5, 26);
+
+            mediaList.Add(Zom100_V1);
         }
     }
 }
---
+++ Timeliner/MediaTypes/Media.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timeliner.MediaTypes
+{
+    //Base class for all media forms - books, movie, TV etc. 
+    internal class Media
+    {
+        public Media()
+        {
+
+        }
+
+        /// <summary>Date & time when the media was first began</summary>
+        public DateTime startDateTime { get; set; }
+        /// <summary>Date & time when the media was completed</summary>
+        public DateTime finishDateTime { get; set; }
+
+        public string name { get; set; }
+    }
+}
